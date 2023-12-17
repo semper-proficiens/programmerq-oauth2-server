bdc4bec1fb36d368edd4e2fedf901acda42fe442
nubesFilius <eperezlarosa@outlook.com>
2023-12-17 13:24:51 -0500

    Updating html and js files

--- static/scripts/user.js
+++ static/scripts/user.js
@@ -1,5 +1,11 @@
+// Document ready event ensures that code runs after DOM is loaded
+// code to remove the cookie from a logged-in user
 $(document).ready(function () {
+
+    // Setting up click event handler for elements with 'btn-logout' class
     $('.btn-logout').click(function (e) {
+
+        // Removing 'auth-session' cookie
         Cookies.remove('auth-session');
     });
 });
